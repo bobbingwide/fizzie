5e0b99c880f61b19b380d05fed1db16ac0745666
Herb Miller <herb@bobbingwide.com>
2020-11-06 07:41:06 +0000

    Issue #22 - start implementing menu improvements - by fiddling url and className attributes

--- functions.php
+++ functions.php
@@ -188,6 +188,18 @@ function fizzie_register_block_type_args( $args ) {
             $args['render_callback'] = 'fizzie_render_block_core_template_part';
         }
     }
+
+    if ( 'core/navigation' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_navigation' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_navigation';
+        }
+    }
+
+    if ( 'core/navigation-link' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_navigation_link' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_navigation_link';
+        }
+    }
     return $args;
 }
 
@@ -320,4 +332,59 @@ function fizzie_render_block_core_template_part( $attributes, $content, $block )
 
     $html = fizzie_lazy_render_block_core_template_part( $attributes, $content, $block );
     return $html;
+}
+
+function fizzie_render_block_core_navigation( $attributes, $content, $block ) {
+    //bw_trace2();
+    $html = gutenberg_render_block_core_navigation( $attributes, $content, $block );
+    bw_trace2( $html, "html");
+    return $html;
+}
+
+function fizzie_render_block_core_navigation_link( $attributes, $content, $block ) {
+    $attributes = fizzie_fiddle_nav_atts( $attributes );
+    $html = gutenberg_render_block_core_navigation_link($attributes, $content, $block);
+    return $html;
+}
+
+/**
+ * Fiddles navigation-link attributes to get the required CSS classes.
+ *
+ * This is much simpler than the logic in `_wp_menu_item_classes_by_context( $menu_items )`.
+ *
+ * @TODO But, we also need to set the other classes such as current-menu-ancestor
+ * which probably means we have to do this in core/navigation rather than core/navigation-link
+ *
+ * @param $attributes
+ * @return mixed
+ */
+function fizzie_fiddle_nav_atts( $attributes ) {
+    $attributes['url'] = str_replace( 'https://s.b/wp56', site_url(), $attributes['url'] );
+    $request_uri = $_SERVER["REQUEST_URI"];
+    $request_uri_path = parse_url( $request_uri, PHP_URL_PATH );
+    $url_path = parse_url( $attributes['url'], PHP_URL_PATH );
+    $site_url = trailingslashit( parse_url( site_url(), PHP_URL_PATH ) );
+    /*
+    echo $request_uri_path;
+    echo ' ';
+    echo $url_path;
+    echo ' ';
+    echo $site_url;
+    echo '<br/>';
+    */
+    // We need to avoid the home page: home_url() or site_url()
+    if ( $url_path === $site_url ) {
+        if ( $url_path === $request_uri_path ) {
+            $attributes['className'] = ['current-menu-item'];
+        } else {
+            // don't match this $url path with all the other paths
+
+        }
+    } elseif ( 0 === strpos( $request_uri, $url_path ) ) {
+
+    // @TODO check that the attributes URL is
+    //if ( parse_url( $request_uri, PHP_URL_PATH ) === parse_url( $attributes['url'], PHP_URL_PATH ) ) {
+        $attributes['className'] = ['current-menu-item'];
+    }
+    return $attributes;
 }
