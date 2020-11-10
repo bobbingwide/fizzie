e186112f2ed2da43c7e33a78b4b786a931918cbb
Herb Miller <herb@bobbingwide.com>
2020-11-10 13:37:55 +0000

    Issue #29 - core/post-hierarchical terms - cater for unknown taxonomies

--- functions.php
+++ functions.php
@@ -224,6 +224,12 @@ function fizzie_register_block_type_args( $args ) {
             $args['render_callback'] = 'fizzie_render_block_core_navigation_link';
         }
     }
+
+	if ( 'core/post-hierarchical-terms' == $args['name'] ) {
+		if ( 'gutenberg_render_block_core_post_hierarchical_terms' == $args['render_callback'] ) {
+			$args['render_callback'] = 'fizzie_render_block_core_post_hierarchical_terms';
+		}
+	}
     return $args;
 }
 
@@ -447,4 +453,28 @@ function fizzie_fiddle_nav_atts( $attributes ) {
         $attributes['className'] = ['current-menu-item'];
     }
     return $attributes;
+}
+
+/**
+ * Renders the `core/post-hierarchical-terms` block on the server.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ * @return string Returns the filtered post hierarchical terms for the current post wrapped inside "a" tags.
+ */
+function fizzie_render_block_core_post_hierarchical_terms( $attributes, $content, $block ) {
+	//bw_trace2();
+	if ( ! isset( $block->context['postId'] ) || ! isset( $attributes['term'] ) ) {
+		return '';
+	}
+
+	$post_hierarchical_terms = get_the_terms( $block->context['postId'], $attributes['term'] );
+	//bw_trace2( $post_hierarchical_terms, "pht", false );
+	if ( is_wp_error( $post_hierarchical_terms ) ) {
+		return 'Taxonomy not recognised';
+	}
+	$html=gutenberg_render_block_core_post_hierarchical_terms( $attributes, $content, $block );
+
+	return $html;
 }
