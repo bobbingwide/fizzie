c615928a637a7777f8e5557581a7929780ebf870
Herb Miller <herb@bobbingwide.com>
2020-11-13 18:47:13 +0000

    Issue #31 - attempt to allow recursive reusable block problems to be fixed using the editor. Issue #25 - start refactoring of overrides

---
+++ includes/block-override-functions.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Implements common functions used in block overrides
+ *
+ */
+
+/**
+ * Determines whether or not to process this content.
+ *
+ * @param string|integer Unique ID for the content
+ * @return bool - true if the post has not been processed. false otherwise
+ */
+function fizzie_process_this_content( $id  ) {
+    global $fizzie_processed_content;
+    // Use this rather than context['postId']
+    global $post;
+    $fizzie_processed = bw_array_get( $fizzie_processed_content, $id, false );
+
+    if ( empty( $fizzie_processed_content ) ) {
+        $global_id = isset( $post ) ? $post->ID : null;
+        if ( $global_id ) {
+            $fizzie_processed_content[ $global_id ] = $global_id;
+        }
+    }
+
+
+    if ( !$fizzie_processed ) {
+        $fizzie_processed_content[$id] = $id ;
+    }
+
+    /** Stop when it looks highly likely we've missed something */
+    if ( count( $fizzie_processed_content ) > 10 ) {
+        $fizzie_processed = true;
+
+    }
+    bw_trace2( $fizzie_processed_content, "processed posts", true, BW_TRACE_DEBUG );
+    return( !$fizzie_processed );
+}
+
+/**
+ * Pops or clears the array of processed content.
+ *
+ * As we return to the previous level we can clear the processed content.
+ * Basically this is something we have to do while processing certain inner blocks:
+ *
+ * - core/post-content
+ * - core/template-part
+ * - core/post-excerpt - possibly
+ * - core/block - possibly
+ *
+ * Note: The top level is within the template, which loads the template parts and/or queries.
+ */
+function fizzie_clear_processed_content( $id=null ) {
+    global $fizzie_processed_content;
+    if ( $id ) {
+        array_pop( $fizzie_processed_content );
+    } else {
+        $fizzie_processed_content = array();
+    }
+    bw_trace2( $fizzie_processed_content, "cleared", false, BW_TRACE_DEBUG );
+}
+
+/**
+ * Reports a recursion error to the user.
+ *
+ * If WP_DEBUG is true then additional information is displayed.
+ *
+ * @param $id string|integer recursive ID detected
+ * @param $type string content type
+ * @return string
+ */
+function fizzie_report_recursion_error( $id, $type='core/post-content') {
+    bw_trace2();
+    bw_backtrace();
+    global $bad_id, $bad_fizzie_processed_content, $fizzie_processed_content;
+    $bad_id = $id;
+    $bad_fizzie_processed_content = $fizzie_processed_content;
+
+    $content = array();
+    $content[] = '<div class="recursion-error">';
+    switch ( $type ) {
+        case 'core/post-content':
+            $content[] = __( 'Content not available; already processed.', 'fizzie' );
+            break;
+        case 'core/template-part':
+            $content[] = __( 'Template part not processed to avoid infinite recursion', 'fizzie');
+            break;
+        case 'core/block':
+            $content[] = __( 'Reusable block not processed to avoid infinite recursion', 'fizzie');
+            fizzie_add_filter_rest_prepare_wp_block();
+            break;
+        default:
+            $content[] = __( 'Infinite recursion error prevented', 'fizzie');
+    }
+    if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+        $content[] = "<span class=\"recursion-error-context $type\">";
+        $content[] = '<br />';
+        $content[] = $id;
+        $content[] = '<br />';
+        $content[] = $type;
+        $content[] = '<br />';
+        global $fizzie_processed_content;
+        $content[] = implode( ',', $fizzie_processed_content );
+        $content[] = '</span>';
+    }
+    $content[] = '</div>';
+    $content = implode( " \n", $content);
+    return $content;
+}
+
+function fizzie_add_filter_rest_prepare_wp_block() {
+    // We only need to do this in REST API processing
+    // But this will only be called in REST API processing
+    // So no need to check
+    add_filter('rest_prepare_wp_block', 'fizzie_rest_prepare_wp_block', 10, 3);
+}
+
+/**
+ *
+ * Fiddle with the raw content to remove the wp:block block.
+ *
+ * [raw] => <!-- wp:paragraph -->
+<p>This is reusable block 21117</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:block {"ref":1134} /-->
+ * @param $response
+ * @param $post
+ * @param $request
+ * @return mixed#
+ */
+function fizzie_rest_prepare_wp_block( $response, $post, $request ) {
+    bw_trace2();
+    bw_backtrace();
+    global $bad_id;
+    global $bad_fizzie_processed_content;
+    $content = $response->data['content']['raw'];
+    $content = fizzie_replace_bad( $content, $bad_id );
+    foreach ( $bad_fizzie_processed_content as $id ) {
+        $content = fizzie_replace_bad( $content, $id );
+    }
+    // Convert any remaining to a missing block
+    $content = str_replace( "wp:block {", "missing {", $content );
+
+    $response->data['content']['raw'] = $content;
+    bw_trace2(  $response, "response", false );
+    return $response;
+}
+
+function fizzie_replace_bad( $content, $bad_id ) {
+    $replacement_block = "<!-- wp:paragraph -->";
+    $replacement_block .= sprintf(__('Error: Recursion was detected while loading the reusable block with post ID %1$s. '), $bad_id);
+    $replacement_block .= fizzie_title_link($bad_id);
+    $replacement_block .= '<br />';
+    $replacement_block .= "Recommendation: Please delete this block.";
+    $replacement_block .= "<!-- /wp:paragraph -->";
+    $content = str_replace("<!-- wp:block {\"ref\":$bad_id} /-->", $replacement_block, $content);
+    return $content;
+}
+
+
+function fizzie_title_link( $id  ) {
+    $link = sprintf( '<a href="%s">%s</a>', esc_url( get_permalink( $id ) ), esc_html( get_the_title( $id ) ) );
+    return $link;
+}
+
+
+
---
+++ includes/block-overrides.php
@@ -0,0 +1,331 @@
+<?php
+/**
+ * Implements block overrides as required.
+ *
+ */
+require_once __DIR__ . '/block-override-functions.php';
+
+/**
+ * Here we include only the blocks we want to override
+ * Either comment out the ones that aren't needed any more
+ * or find another way of detecting whether or not to include the file
+ */
+//require_once __DIR__  . '/query-pagination.php';
+
+/*
+ * fizzie_maybe_override_block( 'core/query-pagination', 'gutenberg_render_block_core_query_pagination' );
+ */
+
+
+/**
+ * Hook into register_block_types_args before WP_Block_Supports
+ */
+add_filter( 'register_block_type_args', 'fizzie_register_block_type_args', 9 );
+
+function fizzie_register_block_type_args( $args ) {
+    if ( 'core/query-pagination' == $args['name']) {
+        if ( 'gutenberg_render_block_core_query_pagination' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_query_pagination';
+        }
+    }
+    if ( 'core/query-loop' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_query_loop' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_query_loop';
+        }
+    }
+
+    if ( 'core/post-excerpt' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_post_excerpt' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_post_excerpt';
+        }
+    }
+
+    if ( 'core/post-content' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_post_content' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_post_content';
+        }
+    }
+
+    if ( 'core/template-part' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_template_part' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_template_part';
+        }
+    }
+
+    if ( 'core/navigation' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_navigation' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_navigation';
+        }
+    }
+
+    if ( 'core/navigation-link' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_navigation_link' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_navigation_link';
+        }
+    }
+
+    if ( 'core/post-hierarchical-terms' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_post_hierarchical_terms' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_post_hierarchical_terms';
+        }
+    }
+
+    if ( 'core/block' == $args['name'] ) {
+        if ( 'gutenberg_render_block_core_block' == $args['render_callback'] ) {
+            $args['render_callback'] = 'fizzie_render_block_core_block';
+        }
+    }
+
+    return $args;
+}
+
+/**
+ * Overrides core/query-pagination to implement main query pagination.
+ *
+ * Hack until a solution is delivered in Gutenberg.
+ *
+ * @param $attributes
+ * @param $content
+ * @param $block
+ * @return string
+ */
+function fizzie_render_block_core_query_pagination( $attributes, $content, $block ) {
+    if ( isset( $block->context['queryId'] ) ) {
+        $html = gutenberg_render_block_core_query_pagination( $attributes, $content, $block );
+    } else {
+        $html = fizzie_render_block_core_query_pagination_main_query( $attributes, $content, $block );
+    }
+    return $html;
+}
+
+/**
+ * Overrides core/query-loop to implement main query processing.
+ *
+ * Hack until a solution is delivered in Gutenberg.
+ *
+ * @param $attributes
+ * @param $content
+ * @param $block
+ * @return string
+ */
+function fizzie_render_block_core_query_loop( $attributes, $content, $block ) {
+    if ( isset( $block->context['queryId'] ) ) {
+        $html = gutenberg_render_block_core_query_loop( $attributes, $content, $block );
+    } else {
+        $html = fizzie_render_block_core_query_loop_main_query( $attributes, $content, $block );
+    }
+    return $html;
+}
+
+/**
+ * Renders the `core/query-pagination` block on the server for the main query.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Returns the pagination for the query.
+ */
+function fizzie_render_block_core_query_pagination_main_query( $attributes, $content, $block ) {
+    $html = '<div class="wp-block-query-pagination">';
+    $html .= paginate_links( [ 'type' => 'list'] );
+    $html .= "</div>";
+    return $html;
+}
+
+/**
+ * Renders the `core/query-loop` block for the main query on the server.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Returns the output of the query, structured using the layout defined by the block's inner blocks.
+ */
+function fizzie_render_block_core_query_loop_main_query( $attributes, $content, $block ) {
+    if ( have_posts() ) {
+        $content = '';
+        while ( have_posts() ) {
+            the_post();
+            $post = get_post();
+            $content .= (
+            new WP_Block(
+                $block->parsed_block,
+                array(
+                    'postType' => $post->post_type,
+                    'postId' => $post->ID,
+                )
+            )
+            )->render(array('dynamic' => false));
+        }
+    } else {
+        $content = __( "No posts found." );
+    }
+    return $content;
+}
+
+/**
+ * Appends the missing </div> to the core/post-excerpt block.
+ *
+ * @param $attributes
+ * @param $content
+ * @param $block
+ * @return string
+ */
+function fizzie_render_block_core_post_excerpt( $attributes, $content, $block ) {
+    $html = gutenberg_render_block_core_post_excerpt( $attributes, $content, $block );
+    // Should really check that it's missing.
+    if ( 0 !== strrpos( $html, '</div>') ) {
+        $html .= '</div>';
+    }
+    return $html;
+}
+
+/**
+ * Overrides core/post-content to return early in certain circumstances.
+ *
+ * Hack until a solution is delivered in Gutenberg.
+ *
+ * @param $attributes
+ * @param $content
+ * @param $block
+ * @return string
+ */
+function fizzie_render_block_core_post_content( $attributes, $content, $block ) {
+    if ( ! isset( $block->context['postId'] ) ) {
+        return '';
+    }
+    /*
+    if ( 'revision' === $block->context['postType'] ) {
+        return '';
+    }
+    */
+
+    if ( fizzie_process_this_content( get_the_ID() ) ) {
+        $html = gutenberg_render_block_core_post_content( $attributes, $content, $block );
+        fizzie_clear_processed_content( get_the_ID() );
+    } else {
+        $html = fizzie_report_recursion_error( get_the_ID() );
+        //$html .= $content;
+    }
+
+    return $html;
+}
+
+
+
+/**
+ * Overrides core/template-part to return early in certain circumstances.
+ *
+ * Hack until a solution is delivered in Gutenberg.
+ *
+ * @param $attributes
+ * @param $content
+ * @param $block
+ * @return string
+ */
+function fizzie_render_block_core_template_part( $attributes, $content, $block ) {
+    require_once __DIR__ . '/template-part.php';
+    $html = fizzie_lazy_render_block_core_template_part( $attributes, $content, $block );
+    return $html;
+}
+
+function fizzie_render_block_core_navigation( $attributes, $content, $block ) {
+    //bw_trace2();
+    $html = gutenberg_render_block_core_navigation( $attributes, $content, $block );
+    bw_trace2( $html, "html");
+    return $html;
+}
+
+function fizzie_render_block_core_navigation_link( $attributes, $content, $block ) {
+    $attributes = fizzie_fiddle_nav_atts( $attributes );
+    $html = gutenberg_render_block_core_navigation_link($attributes, $content, $block);
+    return $html;
+}
+
+/**
+ * Fiddles navigation-link attributes to get the required CSS classes.
+ *
+ * This is much simpler than the logic in `_wp_menu_item_classes_by_context( $menu_items )`.
+ *
+ * @TODO But, we also need to set the other classes such as current-menu-ancestor
+ * which probably means we have to do this in core/navigation rather than core/navigation-link
+ *
+ * @param $attributes
+ * @return mixed
+ */
+function fizzie_fiddle_nav_atts( $attributes ) {
+    $attributes['url'] = str_replace( 'https://s.b/wp56', site_url(), $attributes['url'] );
+    $request_uri = $_SERVER["REQUEST_URI"];
+    $request_uri_path = parse_url( $request_uri, PHP_URL_PATH );
+    $url_path = parse_url( $attributes['url'], PHP_URL_PATH );
+    $site_url = trailingslashit( parse_url( site_url(), PHP_URL_PATH ) );
+    /*
+    echo $request_uri_path;
+    echo ' ';
+    echo $url_path;
+    echo ' ';
+    echo $site_url;
+    echo '<br/>';
+    */
+    // We need to avoid the home page: home_url() or site_url()
+    if ( $url_path === $site_url ) {
+        if ( $url_path === $request_uri_path ) {
+            $attributes['className'] = ['current-menu-item'];
+        } else {
+            // don't match this $url path with all the other paths
+
+        }
+    } elseif ( 0 === strpos( $request_uri, $url_path ) ) {
+
+        // @TODO check that the attributes URL is
+        //if ( parse_url( $request_uri, PHP_URL_PATH ) === parse_url( $attributes['url'], PHP_URL_PATH ) ) {
+        $attributes['className'] = ['current-menu-item'];
+    }
+    return $attributes;
+}
+
+/**
+ * Renders the `core/post-hierarchical-terms` block on the server.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ * @return string Returns the filtered post hierarchical terms for the current post wrapped inside "a" tags.
+ */
+function fizzie_render_block_core_post_hierarchical_terms( $attributes, $content, $block ) {
+    //bw_trace2();
+    if ( ! isset( $block->context['postId'] ) || ! isset( $attributes['term'] ) ) {
+        return '';
+    }
+
+    $post_hierarchical_terms = get_the_terms( $block->context['postId'], $attributes['term'] );
+    //bw_trace2( $post_hierarchical_terms, "pht", false );
+    if ( is_wp_error( $post_hierarchical_terms ) ) {
+        return 'Taxonomy not recognised';
+    }
+    $html=gutenberg_render_block_core_post_hierarchical_terms( $attributes, $content, $block );
+
+    return $html;
+}
+/**
+ * Renders the `core/block` block on server.
+ *
+ * @param array $attributes The block attributes.
+ *
+ * @return string Rendered HTML of the referenced block.
+ */
+function fizzie_render_block_core_block( $attributes, $content, $block ) {
+    bw_trace2();
+       if ( empty( $attributes['ref'] ) ) {
+        return '';
+    }
+    if ( fizzie_process_this_content( $attributes['ref'] ) ) {
+        $html = gutenberg_render_block_core_block( $attributes, $content, $block );
+        fizzie_clear_processed_content( $attributes['ref'] );
+    } else {
+        $html = fizzie_report_recursion_error( $attributes['ref'], 'core/block' );
+    }
+    bw_trace2( $html, "html", false);
+    return $html;
+}
